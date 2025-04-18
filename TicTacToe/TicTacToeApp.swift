01b1bd6051fc9dbb108a1435d5ece17d40da468e
Ivan Telleria <ivantelleria96@gmail.com>
2025-04-18 18:22:01 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Ivan Telleria on 18/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
